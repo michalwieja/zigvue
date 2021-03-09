1051bb75bf6119f8622c89617d4ef21d861892b9
Michał Wieja <m.wieja@qarbon.pl>
2021-03-09 14:29:11 +0100

    hero in progress

---
+++ src/components/Hero.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="hero">
+    <div class="hero__wrapper">
+      <div class="hero__content">
+        <div class="hero__title title">TU SĄ
+          MOŻLIWOŚCI
+        </div>
+        <div class="hero__text">Sukcesu w biznesie nie buduje się w pojedynkę, dlatego tworzymy
+          społeczność, która daje wartość
+        </div>
+        <div class="button-wrapper">
+          <button class="button button--light-blue">dołącz</button>
+          <button class="button button--light-blue">więcej</button>
+        </div>
+        <div class="socials-wrapper">
+          <div class="fb">FB</div>
+          <div class="in">IN</div>
+        </div>
+
+
+      </div>
+      <div class="hero__image">
+        <img alt="" src="">
+        <div class="description">
+          <div class="descriptio__title">
+            Zegarmistrzowska tradycja rodzinna
+          </div>
+          <div class="description__text">
+            Dominik Sibielak. Dąbrowski zegarmistrz. Właściciel zakładu mieszczącego się w Dąbrowie
+            Górniczej przy ul. Kasprzaka 1A. Pracowity człowiek, z pasją poświęcający się swoim
+            zajęciom. Tchnie nowe życie w każdy zegarek, który klient mu przyniesie.
+          </div>
+          <button>czytaj wiecej</button>
+
+
+        </div>
+      </div>
+    </div>
+
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Hero"
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,18 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Hero/>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+
+import Hero from '@/components/Hero';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { Hero }
 }
 </script>
-
-<style lang="scss">
-
-</style>
